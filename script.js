96c46e0fa31d97d167cad5da601cf54df73d4226
Gabriel Lantin <luigilantin@gmail.com>
2021-09-30 02:23:32 -0700

    started color changing time blocks

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-// var now = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+var now = moment();
+
+var timeBlocks = document.getElementsByClassName("time-block");
+console.log(timeBlocks)
 
 setInterval(function () {
     $('#currentDay').html(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"))
 }, 0);
 
+
+// fix -- also check for am pm
+for (let i = 0; i < timeBlocks.length; i++) {
+    var str = timeBlocks.item(i).innerHTML;
+    var hourStr = str.replace(/\D/g, "").slice(0, -2);
+    var hour = parseInt(hourStr);
+    console.log(hour, now.hour())
+    if (hour < 3) {
+        $(".description").addClass("past");
+    } else if (hour > 3) {
+        $(".description").addClass("future");
+    } else {
+        $(".description").addClass("present");
+    }
+}
